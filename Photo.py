586af7a3e9f4a5bb33514e19e7dca198e95e31b8
allnc <meibrohimov@gmail.com>
2023-11-27 21:16:31 +0500

    all album file

--- Photo.py
+++ Photo.py
@@ -7,9 +7,9 @@ album_builder.add(type="photo", media=FSInputFile("./img/ip4.webp")) # input loc
 album_builder.add(type="photo", media=FSInputFile("./img/ip2.jpg")) # input local file
 album_builder.add(type="photo", media=FSInputFile("./img/ip3.jpg")) # input local file
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6") # input global file
-album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
-album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
-album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6") # input global file
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6") # input global file
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6") # input global file
 # await message.answer_media_group(
 #     # Не забудьте вызвать build()
 #     media=album_builder.build()
