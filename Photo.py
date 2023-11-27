a6148b1c9bdb453b7eb3758ace3eaa87d799d2b6
allnc <meibrohimov@gmail.com>
2023-11-27 21:16:18 +0500

    all album file

--- Photo.py
+++ Photo.py
@@ -5,8 +5,8 @@ album_builder = MediaGroupBuilder(caption="Apple iphone 14 pro max style")
 album_builder.add(type="photo", media=FSInputFile("./img/ip1.webp")) # input local file
 album_builder.add(type="photo", media=FSInputFile("./img/ip4.webp")) # input local file
 album_builder.add(type="photo", media=FSInputFile("./img/ip2.jpg")) # input local file
-album_builder.add(type="photo", media=FSInputFile("./img/ip3.jpg"))
-album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
+album_builder.add(type="photo", media=FSInputFile("./img/ip3.jpg")) # input local file
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6") # input global file
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
