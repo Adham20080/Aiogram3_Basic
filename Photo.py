df05c85119d85075dac8ca85892a6bf547f0ebf8
allnc <meibrohimov@gmail.com>
2023-11-27 21:16:01 +0500

    all album file

--- Photo.py
+++ Photo.py
@@ -2,9 +2,9 @@ from aiogram.types import FSInputFile
 from aiogram.utils.media_group import MediaGroupBuilder
 
 album_builder = MediaGroupBuilder(caption="Apple iphone 14 pro max style")
-album_builder.add(type="photo", media=FSInputFile("./img/ip1.webp"))
-album_builder.add(type="photo", media=FSInputFile("./img/ip4.webp"))
-album_builder.add(type="photo", media=FSInputFile("./img/ip2.jpg"))
+album_builder.add(type="photo", media=FSInputFile("./img/ip1.webp")) # input local file
+album_builder.add(type="photo", media=FSInputFile("./img/ip4.webp")) # input local file
+album_builder.add(type="photo", media=FSInputFile("./img/ip2.jpg")) # input local file
 album_builder.add(type="photo", media=FSInputFile("./img/ip3.jpg"))
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
 album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
