08a828fc924f22c8cc4a2942d6d3ffe5bcb02619
allnc <meibrohimov@gmail.com>
2023-11-27 21:09:51 +0500

    all album file

--- Photo.py
+++ Photo.py
@@ -2,11 +2,14 @@ from aiogram.types import FSInputFile
 from aiogram.utils.media_group import MediaGroupBuilder
 
 album_builder = MediaGroupBuilder(caption="Apple iphone 14 pro max style")
-album_builder.add(type="photo", media=FSInputFile("image_from_pc.jpg"))
-album_builder.add_photo(media="https://images.app.goo.gl/6faejXkHcnCcw37s8")
-album_builder.add_photo(media="https://images.app.goo.gl/cQw9iKqvmg6euYva9")
-album_builder.add_photo(media="https://images.app.goo.gl/x4hBA1Nxa69kk15h9")
-album_builder.add_photo(media="https://images.app.goo.gl/W9SkjvnwBDcgnKeP8")
+album_builder.add(type="photo", media=FSInputFile("./img/ip1.webp"))
+album_builder.add(type="photo", media=FSInputFile("./img/ip4.webp"))
+album_builder.add(type="photo", media=FSInputFile("./img/ip2.jpg"))
+album_builder.add(type="photo", media=FSInputFile("./img/ip3.jpg.webp"))
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
+album_builder.add_photo(media="https://images.app.goo.gl/QeCLLpW4WpN1NVnj6")
 # await message.answer_media_group(
 #     # Не забудьте вызвать build()
 #     media=album_builder.build()
